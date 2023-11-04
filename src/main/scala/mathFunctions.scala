ca7f0696e3824b8a00e778fa39efe23e364fefb1
HARSHIDA SHAILY <88976726+HARSHIDA18@users.noreply.github.com>
2023-11-04 14:17:24 +0530

    Update mathFunctions.scala

--- src/main/scala/mathFunctions.scala
+++ src/main/scala/mathFunctions.scala
@@ -1,3 +1,25 @@
-object mathFunctions {
+class Calculator
+{
+  def add(a:Int,b:Int):Int=a+b
+  def subtract(a:Int,b:Int):Int={
+    if   (a>b) a-b else b-a
+  }
+  def multiplication(a:BigInt,b:BigInt)=a*b
+
+  def  division(a:Double,b:Double):Double={
+    if   (b==0) 0.0
+    else a/b
+  }
+  def modulus(a:Int,b:Int):Int={
+    a%b
+  }
 
 }
+object mathFunctions extends App{
+  val calc=new Calculator
+  println((calc.add(10,20)))
+  println((calc.subtract(10,20)))
+  println((calc.multiplication(10,20)))
+  println((calc.division(10,20)))
+}
+
